68d3869f26d6a98f78f647ac23fad1e0e216d309
gtkashi <greatteacherkashi@gmail.com>
2022-12-17 11:59:39 -0800

    Invisibility fixes
    Found some issues with getting the player to be fully opaque again when using invisible. Seems the trigger for character model visibility is tied to the name plate visibility. Talking to an NPC would fix it, but that's not ideal. For now, the name plate will flash if buff ID 69 is dropped. There may be some other buff IDs that might need to get checked for, but trying this for now.
    
    Also added a similar brief nameplate flash when the addon is loaded, so reloading the app properly resets itself.
    
    Name visibility is now reset when unloading the app.

--- nameless.lua
+++ nameless.lua
@@ -1,22 +1,56 @@
 _addon.name = 'nameless'
-_addon.version = '0.1.0'
+_addon.version = '0.1.1'
 _addon.author = 'GTKashi, distilled from revisible 0.9.2 by Darkdoom;Rubenator;Akaden'
 
+local self_invisible_flag = 0x8
 local packets = require('packets')
 local bit = require('bit')
 require('sets')
+local config = require('config')
+ 
+local defaults = {
+	debug=true,
+}
 
 local path = windower.addon_path:gsub('\\', '/') .. 'EntityFlagChanger.dll'
 local _FlagChanger = assert(package.loadlib(path, 'luaopen_EntityFlagChanger'))()
+local settings = config.load(defaults)
 
 local hideplayername = function()
-	_FlagChanger.HideEntityName(windower.ffxi.get_player().index)
+	playerindex = windower.ffxi.get_player().index
+	_FlagChanger.HideEntityName(playerindex)
 end
 
-windower.register_event('incoming chunk',function(id, original, modified, injected, blocked)
-  if id == 0x001D then
-	_FlagChanger.HideEntityName(windower.ffxi.get_player().index)
-  end
+local debug = function(message, ...)
+	if settings.debug then
+		print('Nameless >> '..string.format(message, ...))
+	end
+end
+
+windower.register_event('load', function()
+	playerindex = windower.ffxi.get_player().index
+	_FlagChanger.ShowEntityName(playerindex)
+	coroutine.sleep(0.1)
+	hideplayername()
 end)
 
-windower.register_event('load', hideplayername)
+windower.register_event('incoming chunk',function(id, original, modified, injected, blocked)
+	if id == 0x001D then
+		hideplayername()
+	end
+end)
+
+windower.register_event('lose buff', function(buff_id)
+	playerindex = windower.ffxi.get_player().index
+	--debug(tostring(buff_id))
+	if buff_id == 69 then
+		_FlagChanger.ShowEntityName(playerindex)
+		coroutine.sleep(0.1)
+		_FlagChanger.HideEntityName(playerindex)
+	end
+end)
+
+windower.register_event('unload', function()
+	playerindex = windower.ffxi.get_player().index
+	_FlagChanger.ShowEntityName(playerindex)
+end)
